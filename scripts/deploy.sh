f07e965366eabd4403f45bf392e02ef2e031c7e3
Taro Fukunaga <tarof429@gmail.com>
2020-09-14 16:32:25 -0700

    Build and tag the image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,14 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-GIT_REVISION=$1
+GIT_REVISION="$1"
 
 
 echo "Git revision during deployment phase is '$GIT_REVISION'"
 
-# docker build -t tarof429/multi-client -t tarof429/multi-client-$GIT_REVISION -f ./client/Dockerfile ./client
-# docker build -t tarof429/multi-server -t tarof429/multi-server-$GIT_REVISION -f ./server/Dockerfile ./server
-# docker build -t tarof429/multi-worker -t tarof429/multi-worker-$GIT_REVISION -f ./worker/Dockerfile ./worker
+docker build -t tarof429/multi-client -t tarof429/multi-client-$GIT_REVISION -f ./client/Dockerfile ./client
+docker build -t tarof429/multi-server -t tarof429/multi-server-$GIT_REVISION -f ./server/Dockerfile ./server
+docker build -t tarof429/multi-worker -t tarof429/multi-worker-$GIT_REVISION -f ./worker/Dockerfile ./worker
 # docker push tarof429/multi-client 
 # docker push tarof429/multi-client-$GIT_REVISION 
 # docker push tarof429/multi-server
