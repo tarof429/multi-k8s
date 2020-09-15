7892062fe6c5d5ae52fb9663a3d7676407409d85
Taro Fukunaga <tarof429@gmail.com>
2020-09-15 09:37:02 -0700

    Use travis env section to set global variables

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,10 @@
-echo "Running deploy.sh"
+#!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    echo "deploy.sh: Missing git revision"
-    exit 1
-fi
+echo "Git revision during deployment phase is '$SHA'"
 
-GIT_REVISION="$1"
-
-
-echo "Git revision during deployment phase is '$GIT_REVISION'"
-
-docker build -t tarof429/multi-client -t tarof429/multi-client-$GIT_REVISION -f ./client/Dockerfile ./client
-docker build -t tarof429/multi-server -t tarof429/multi-server-$GIT_REVISION -f ./server/Dockerfile ./server
-docker build -t tarof429/multi-worker -t tarof429/multi-worker-$GIT_REVISION -f ./worker/Dockerfile ./worker
+docker build -t tarof429/multi-client:latest -t tarof429/multi-client-$SHA -f ./client/Dockerfile ./client
+docker build -t tarof429/multi-server:latest -t tarof429/multi-server-$SHA -f ./server/Dockerfile ./server
+docker build -t tarof429/multi-worker:latest -t tarof429/multi-worker-$SHA -f ./worker/Dockerfile ./worker
 # docker push tarof429/multi-client 
 # docker push tarof429/multi-client-$GIT_REVISION 
 # docker push tarof429/multi-server
